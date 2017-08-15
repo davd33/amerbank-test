176b239875453158a3d0848eadd4f13cd3861b86
David Rueda <davd33@gmail.com>
2017-08-15 09:58:35 +0200

    run tests for routes - scripts

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -6,3 +6,10 @@ do
     echo "Testing $ms service"
     node "./services-test/$ms/test.js"
 done
+
+# run routes tests
+for ms in `ls routes-test`
+do
+    echo "Testing $ms route"
+    node "./routes-test/$ms/test.js"
+done
